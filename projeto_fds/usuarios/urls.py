c09dbcd9aef8a1a75f3b897900b1724c7d3fef56
biagalindoo <biagalindo4@gmail.com>
2024-09-19 11:42:09 -0300

    Update urls.py
    urls dos usuarios

--- projeto_fds/usuarios/urls.py
+++ projeto_fds/usuarios/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from .views import cadastro, login, logout1, home
+
+urlpatterns = [
+    path('cadastro/', cadastro, name='cadastro'),
+    path('login/', login, name='login'),
+    path('logout/', logout1, name='logout'),
+    path('home/', home, name='home'),
+]
 
