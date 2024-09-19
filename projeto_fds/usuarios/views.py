ed1e5ab2017acb7e35d9cf11481cd24a85e7da8b
biagalindoo <biagalindo4@gmail.com>
2024-09-19 11:39:32 -0300

    Update views.py

--- projeto_fds/usuarios/views.py
+++ projeto_fds/usuarios/views.py
@@ -1,36 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if username and password:
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.success(request, f'Bem-vindo, {username}!')
+                return render(request,'app.html')  # Redireciona para a página inicial
+            else:
+                messages.error(request, 'Usuário ou senha inválidos.')
+        else:
+            messages.error(request, 'Por favor, preencha todos os campos.')
+
+    return render(request, 'login.html')
+
+
+def logout1(request):
+    logout(request)
+    messages.info(request, 'Você saiu da sua conta.')
+    return redirect('/usuarios/home')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.core.validators import validar_email
 from .models import UserProfile
 
 def cadastro(request):
-    estados_brasil = [
-        ('AC', 'Acre'),
-        ('AL', 'Alagoas'),
-        ('AP', 'Amapá'),
-        ('AM', 'Amazonas'),
-        ('BA', 'Bahia'),
-        ('CE', 'Ceará'),
-        ('DF', 'Distrito Federal'),
-        ('ES', 'Espírito Santo'),
-        ('GO', 'Goiás'),
-        ('MA', 'Maranhão'),
-        ('MT', 'Mato Grosso'),
-        ('MS', 'Mato Grosso do Sul'),
-        ('MG', 'Minas Gerais'),
-        ('PA', 'Pará'),
-        ('PB', 'Paraíba'),
-        ('PR', 'Paraná'),
-        ('PE', 'Pernambuco'),
-        ('PI', 'Piauí'),
-        ('RJ', 'Rio de Janeiro'),
-        ('RN', 'Rio Grande do Norte'),
-        ('RS', 'Rio Grande do Sul'),
-        ('RO', 'Rondônia'),
-        ('RR', 'Roraima'),
-        ('SC', 'Santa Catarina'),
-        ('SP', 'São Paulo'),
-        ('SE', 'Sergipe'),
-        ('TO', 'Tocantins'),
-    ]
+    estados_brasil = UserProfile.ESTADOS_BRASIL
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        numero_telefone = request.POST.get('numero_telefone')
+        estado = request.POST.get('estado')
+
+        if password1 != password2:
+            messages.error(request, 'As senhas não coincidem.')
+            return render(request, 'usuarios/cadastro.html', {'estados': estados_brasil})
+
+       
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Nome de usuário já existe.')
+            return render(request, 'usuarios/cadastro.html', {'estados': estados_brasil})
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'E-mail já está em uso.')
+            return render(request, 'usuarios/cadastro.html', {'estados': estados_brasil})
+
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        UserProfile.objects.create(user=user, numero_telefone=numero_telefone, estado=estado)
+        login(request, user)
+        messages.success(request, 'Cadastro realizado com sucesso!')
+        return redirect('/usuarios/login')
+
+    return render(request, 'cadastro.html', {'estados': estados_brasil})
+
