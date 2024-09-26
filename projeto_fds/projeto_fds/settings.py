d686b7b4123fd553d9f458dc62f23e6657cd9861
Gabriel Araujo Boeckmann e Silva <gabrielboeckmann@gmail.com>
2024-09-26 18:52:04 -0300

    update settings.py

--- projeto_fds/projeto_fds/settings.py
+++ projeto_fds/projeto_fds/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'usuarios',
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'educacao_financeira',
+    'controle_gastos',
 ]
 
 MIDDLEWARE = [
