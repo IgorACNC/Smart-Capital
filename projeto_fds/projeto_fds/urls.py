c30a3b9c71f5200eec6bfcab9e8d5c836f982055
Igor Couto <iacncouto@hotmail.com>
2024-09-30 20:30:34 -0300

    Delete projeto_fds directory

--- projeto_fds/projeto_fds/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('usuarios.urls')),
-    path('controle_gastos/', include('controle_gastos.urls')),
-    path('educacao-financeira/', include('educacao_financeira.urls')),
-]
