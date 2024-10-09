840117cc8ebb96b60caeff9f06f05ca9086eb641
Igor Couto <iacncouto@hotmail.com>
2024-10-09 11:25:51 -0300

    Delete projeto_fds directory

--- projeto_fds/educacao_financeira/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EducacaoFinanceiraConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "educacao_financeira"
--- projeto_fds/planejamento_aposentadoria/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlanejamentoAposentadoriaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'planejamento_aposentadoria'
