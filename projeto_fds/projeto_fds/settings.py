515dcedc0d2dc2d84959bb8c6fa51906c49bc3db
Igor Couto <iacncouto@hotmail.com>
2024-10-10 20:52:24 -0300

    video

--- projeto_fds/projeto_fds/settings.py
+++ projeto_fds/projeto_fds/settings.py
@@ -165,7 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/ 'templates/static']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 # Default primary key field type
