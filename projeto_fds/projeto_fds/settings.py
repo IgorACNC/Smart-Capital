5e7225e871f18da1424770179195ce6057cf83a4
Igor Couto <iacncouto@hotmail.com>
2024-10-14 22:20:02 -0300

    testando vídeo

--- projeto_fds/projeto_fds/settings.py
+++ projeto_fds/projeto_fds/settings.py
@@ -170,7 +170,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+if not DEBUG:
+    STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
