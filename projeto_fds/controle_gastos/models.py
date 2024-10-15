b44531adef1389c94c38430914bded0ce728ec13
Igor Couto <iacncouto@hotmail.com>
2024-10-15 09:30:13 -0300

    update-meta-final

--- projeto_fds/controle_gastos/models.py
+++ projeto_fds/controle_gastos/models.py
@@ -7,5 +7,17 @@ class Gasto(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data = models.DateField()
 
-    def str(self):
+    def __str__(self):
         return f'{self.descricao} - {self.valor}'
+
+
+class MetaGasto(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    valor_meta = models.DecimalField(max_digits=10, decimal_places=2)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Meta de {self.valor_meta}'
+
+    def checar_valor_ultrapassado(self, total_gastos):
+        return total_gastos >= self.valor_meta
